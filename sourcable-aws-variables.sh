3ac4d0c77959bc906a5b33dc36e64032c8c45f7c
Paul Baker <paul.baker@wgu.edu>
2018-06-28 22:53:37 -0600

    Convienience file

---
+++ sourcable-aws-variables.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# A convienience file that lets me set these variables for a given
+# terminal session. If you're not in the know, run "source sourcable-aws-variables.sh"
+# to set these variables for your current session.
+
+# Set the variables
+AWS_REGION=us-west-2
+AWS_PROFILE=paulbaker
+
+# Export them so the terminal you're currently using has access to these values.
+export AWS_REGION
+export AWS_PROFILE
