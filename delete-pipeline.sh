c2f6ba71cb8d80cb60b364ef87f724a16f7919d6
Paul Baker <paul.baker@wgu.edu>
2018-06-29 03:34:24 -0600

    Logging statement for readability

--- delete-pipeline.sh
+++ delete-pipeline.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 source sourcable-aws-variables.sh
+echo "Deleting CloudFormation stack"
 aws cloudformation delete-stack --stack-name qtest-library-pipeline
--- deploy-pipeline.sh
+++ deploy-pipeline.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source sourcable-aws-variables.sh
-aws cloudformation create-stack --stack-name qtest-library-pipeline --template-body=file://./cloudformation-template.yml
+aws cloudformation create-stack --stack-name qtest-library-pipeline --template-body=file://./cloudformation-template.yml --capabilities CAPABILITY_NAMED_IAM
